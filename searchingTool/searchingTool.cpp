62ae08a774796f208d3819aec85c3b38a4736b67
lscatfish <2561925435@qq.com>
2025-08-29 00:50:44 +0800

    更新搜索工具，支持 Excel 文件解析
    在 `searchingMain.cpp` 中启用主函数，移除控制台分割和业务线程实现，改为创建 `SearchingTool` 对象。
    
    在 `searchingTool.cpp` 中调整头文件包含顺序，添加对 `xlnt` 库的支持，并注释掉部分解析函数以便后续实现。
    
    在 `searchingTool.hpp` 中添加对 `xlnt` 的支持，简化 PDF 文件处理逻辑，新增 `xlsxList_`、`pdfList_` 和 `docxList_` 成员变量，并实现统一的 `parse_list` 函数。

--- searchingTool/searchingTool.cpp
+++ searchingTool/searchingTool.cpp
@@ -5,7 +5,41 @@
 #include <helper.h>
 #include <iostream>
 #include <pdf.h>
-#include<string>
-#include<searchingTool/searchingTool.hpp>
+#include <searchingTool/searchingTool.hpp>
+#include <string>
+#include <xlnt/workbook/workbook.hpp>
 
 
+
+//// 解析xlsx
+//void SearchingTool::parse_xlsx_list( ) {
+//    list< std::string > u8PathList  = this->get_u8filepath_list(list< std::string >{ ".xlsx" });
+//    list< std::string > sysPathList = this->get_filepath_list(list< std::string >{ ".xlsx" });
+//
+//    for (size_t i = 0; i < sysPathList.size( ); i++) {
+//        TextList< xlnt::workbook > afile(sysPathList[i], u8PathList[i]);
+//        xlsxList_.push_back(afile);
+//    }
+//}
+//
+//// 解析pdf
+//void SearchingTool::parse_pdf_list( ) {
+//    list< std::string > u8PathList  = this->get_u8filepath_list(list< std::string >{ ".pdf", ".PDF" });
+//    list< std::string > sysPathList = this->get_filepath_list(list< std::string >{ ".xlsx", ".PDF" });
+//
+//    for (size_t i = 0; i < sysPathList.size( ); i++) {
+//        TextList< pdf::DefPdf > afile(sysPathList[i], u8PathList[i]);
+//        pdfList_.push_back(afile);
+//    }
+//}
+//
+//// 解析docx
+//void SearchingTool::parse_docx_list( ) {
+//    list< std::string > u8PathList  = this->get_u8filepath_list(list< std::string >{ ".pdf", ".PDF" });
+//    list< std::string > sysPathList = this->get_filepath_list(list< std::string >{ ".xlsx", ".PDF" });
+//
+//    for (size_t i = 0; i < sysPathList.size( ); i++) {
+//        TextList< pdf::DefPdf > afile(sysPathList[i], u8PathList[i]);
+//        pdfList_.push_back(afile);
+//    }
+//}
