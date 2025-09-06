e0f6d354eabe14c60aa35a55ff1733892a0f6b96
lscatfish <2561925435@qq.com>
2025-09-06 19:11:58 +0800

    可以考虑写一下表格识别的拓展程序

---
+++ include/ex.h
@@ -0,0 +1,10 @@
+﻿/*****************************************************
+ *
+ * @file 此文件用于拓展docx、xlsx、pdf识别的相关功能
+ *
+ *****************************************************/
+#include <basic.hpp>
+#include <Files.h>
+#include <pdf.h>
+#include <QingziClass.h>
+#include <word.h>
